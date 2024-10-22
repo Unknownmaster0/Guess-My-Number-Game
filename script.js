e1257b9e345c96ab5bfc3731e975a8317891a546
Unknownmaster0 <sagar192002singh@gmail.com>
2024-10-22 21:14:47 +0530

    updated the final score from 20 to 100, and some readability in html and css files.

--- script.js
+++ script.js
@@ -1,36 +1,5 @@
 'use strict';
 
-// now we are going to learn about the 'DOM manipultaion'
-// how to select the html element do change and print to console.
-/*
-// HERE WE SELECTED THE ELEMENT AND PRINT THEIR INSIDE CONTENT.
-console.log(document.querySelector('.message').textContent);
-console.log(document.querySelector('.label-score').textContent);
-console.log(document.querySelector('.score').textContent);
-console.log(document.querySelector('.label-highscore').textContent);
-console.log(document.querySelector('.between').textContent);
-console.log(document.querySelector('h1').textContent);
-
-// NOW WE SELECT THE CONTENT AND CHANGE ITS VALUE TO SOME DIFFERENT VALUE.
-document.querySelector('.message').textContent =
-  'Hii friends 👋 lets start the game.';
-
-document.querySelector('h1').textContent = 'Guess hidden Number.🥷';
-
-// FOR INPUT TAG/ELEMENT
-console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value = 7;
-console.log(document.querySelector('.guess').value);
-*/
-
-/********* Event Listener and Event handler.  ************* */
-//now we can 'do things on some events', like on mouse click,
-
-// console.log(rand);ff
-// console.log(Math.floor(rand)); // consider the only integer part before decimal.
-// console.log(Math.ceil(rand)); // round to next number.
-// console.log(Math.round(rand)); // round to number a/c to rule.
-
 let highscore = 0;
 let rand = Math.floor(Math.random() * 21);
 let score = 10;
@@ -71,7 +40,6 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 
 document.querySelector('.again').addEventListener('click', function () {
-  // intialise all the values, with start except the high score.
   rand = Math.trunc(Math.random() * 21); // mainly cut non-decimal part.
   score = 10;
   document.querySelector('.message').textContent = 'Start guessing...';
